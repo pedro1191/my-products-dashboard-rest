ffaa43c3bf77e55533e6fda300aac3997baea753
pedro1191 <pedro_almeida1191@hotmail.com>
2019-03-15 15:29:36 -0300

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    // return $router->app->version();
+    return "Hello from " . env("API_NAME");
+});
+
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', [
+    'middleware' => ['api.throttle'],
+    'limit' => 100,
+    'expires' => 5,
+    'prefix' => 'api/v1',
+    'namespace' => 'App\Http\Controllers\V1',
+], function ($api) {
+
+});
