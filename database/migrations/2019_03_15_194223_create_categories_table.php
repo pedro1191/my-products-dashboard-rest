eb4ad0484c756abff539c0c515cc77306a41a62d
pedro1191 <pedro_almeida1191@hotmail.com>
2019-03-25 18:52:35 -0300

    Add migration for categories table and update products migration

--- database/migrations/2019_03_15_194223_create_categories_table.php
+++ database/migrations/2019_03_15_194223_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,10 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 100);
-            $table->text('description');
-            $table->longText('image');
             $table->timestamps();
-        });
-
-        Schema::table('products', function (Blueprint $table) {
-            $table->unsignedInteger('category_id');
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');;
         });
     }
 
@@ -34,6 +27,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
     }
 }
